0655908b705efa968fc9250058d912c8c8073fc8
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-05-17 20:46:29 +0200

    exo 1-3 : header for °F-°C table

---
+++ K_and_R/Chapter_1/exo_1-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* print Fahreinheit-Celsius table
+   for fahr = 0, 20, ..., 300, floating-point version
+   with a header for the table
+*/
+
+main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;    /* lower limit of temperature table */
+    upper = 300;  /* upper limit */
+    step = 20;    /* step size */
+
+    fahr = lower;
+
+    printf(" °F  |   °C\n");
+    printf("-----|------\n");
+
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f  |%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
