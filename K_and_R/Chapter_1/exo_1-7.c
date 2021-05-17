cdc62b7803d357f30cbd1c78f4168591515b888c
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-05-17 21:19:46 +0200

    program to get the value of EOF

---
+++ K_and_R/Chapter_1/exo_1-7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* write a program to print the value of EOF */
+
+main() {
+    int end_of_file = EOF;
+
+    printf("The value of EOF is %d\n", end_of_file);
+    /* we get -1 with GCC */
+}
